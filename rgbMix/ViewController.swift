071eee38660a46c3c12bd09ae9cd6c7b04ae9cc2
Tanjerus <Merserus@gmail.com>
2021-12-12 23:00:44 +0300

    home work rib

--- rgbMix/ViewController.swift
+++ rgbMix/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rgbView.layer.cornerRadius = 20
+        rgbView.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        }
+    
+    @IBAction func sliderAction() {
+        let redValue = redSlider.value
+        let greenValue = greenSlider.value
+        let blueValue = blueSlider.value
+        
+        rgbView.backgroundColor = UIColor(red: CGFloat(redValue), green: CGFloat(greenValue), blue: CGFloat(blueValue), alpha: 1)
+        
+        redLabel.text = String(redValue)
+        greenLabel.text = String(greenValue)
+        blueLabel.text = String(blueValue)
     }
-
-
+    
 }
+    
+
 
